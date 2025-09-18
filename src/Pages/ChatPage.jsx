ab09d8becd862746c4402688407d5e7726c65667
lxswami <99294799lx@gmail.com>
2025-09-18 10:47:56 +0530

    fixed the chatting section top bottom padding

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -584,7 +584,7 @@ export default function ChatPage() {
 
       {/* MESSAGES + INPUT WRAPPER */}
       <div className="flex-1 flex flex-col justify-between">
-        <div className="flex-1 overflow-y-auto px-2 md:px-4 py-2">
+        <div className="flex-1 overflow-y-auto px-2 md:px-4 py-2 pt-16 pb-20">
           {loading ? (
             <div className="flex justify-center items-center h-full">
               <div className="w-8 h-8 border-4 border-t-transparent border-indigo-500 rounded-full animate-spin"></div>
@@ -601,7 +601,7 @@ export default function ChatPage() {
         </div>
 
         {/* INPUT FIXED AT BOTTOM */}
-        <div className="fixed bottom-0 w-full bg-white shadow-inner px-2 md:px-4 py-2">
+        <div className="sticky bottom-0 z-20 bg-white shadow-inner px-2 md:px-4 py-2">
           <ChatInput
             newMsg={newMsg}
             setNewMsg={setNewMsg}
