218cb121df8688644125c18f27367ad7da188d3f
lxswami <99294799lx@gmail.com>
2025-09-18 10:49:50 +0530

    fixed the padding

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -584,7 +584,7 @@ export default function ChatPage() {
 
       {/* MESSAGES + INPUT WRAPPER */}
       <div className="flex-1 flex flex-col justify-between">
-        <div className="flex-1 overflow-y-auto px-2 md:px-4 py-2 pt-16 pb-20">
+        <div className="flex-1 overflow-y-auto px-2 md:px-4 pt-16 pb-8">
           {loading ? (
             <div className="flex justify-center items-center h-full">
               <div className="w-8 h-8 border-4 border-t-transparent border-indigo-500 rounded-full animate-spin"></div>
